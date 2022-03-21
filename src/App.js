11194c935d90eba0cc92815298fd1542c54b6d33
Djamess <jamescarvalho8@gmail.com>
2022-03-20 22:53:44 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import './routes/App.routes.js';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const data = Object.fromEntries(formData)
+
+  }
+
+  const [count, setCount] = useState(0);
+
+  return <form onSubmit={handleSubmit}>
+    <h1>Formulário para compra de Pacote de adesivos</h1>
+    <br />
+    <h2> Quais adesivos: </h2>
+
+    <div className="checks">
+      <label>
+        <input id="react" type="checkbox" name="React" />React<br />
+      </label>
+      <label>
+        <input id="vue" type="checkbox" name="Vue" />Vue<br />
+      </label>
+      <label>
+        <input id="angular" type="checkbox" name="Angular" />Angular<br />
+      </label>
     </div>
-  );
+
+    <h2> Quantos adesivos de cada? </h2>
+
+    <button onClick={() => setCount(count - 1)}>-</button>
+    <text id="quantidade" name="quantidade"> {count} </text>
+    <button onClick={() => setCount(count + 1)}>+</button>
+
+    <br />
+    <br />
+
+    <textarea id="obs" name="obs" placeholder='Observações' /><br />
+
+    <button type="submit" href="./routes/App.routes.js">Enviar</button>
+
+  </form>
+
 }
 
 export default App;
---
+++ src/routes/App.routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { ReactDOM } from "react";
+import '../App.css';
+import '../App.js';
+
+function App() {
+    
+    <h1>Formulário para compra de Pacote de adesivos</h1>
+    
+
+}
+
+export default App;
